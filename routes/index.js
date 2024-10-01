0045e0556b38baaabdfa8f23a5ac797ac54207cd
Leornadia <leornadiajose@gmail.com>
2024-10-01 07:58:52 -0700

    index.js

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,28 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
 import AuthController from '../controllers/AuthController';
-import UsersController from '../controllers/UsersController'; // Import UsersController
+import UsersController from '../controllers/UsersController';
+import FilesController from '../controllers/FilesController'; // Make sure this path is correct
 
-const router = express.Router(); // Create a router instance
+const router = express.Router();
 
+// App-related routes
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 
-// Add user creation route
-router.post('/users', UsersController.postNew);  // POST /users
-
-// Add authentication routes
+// Auth-related routes
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
+
+// User-related routes
 router.get('/users/me', UsersController.getMe);
 
-export default router; // Export the router instance
+// File-related routes
+router.post('/files', FilesController.postUpload); // POST route should correctly reference postUpload
+router.get('/files/:id', FilesController.getShow);
+router.get('/files', FilesController.getIndex);
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+export default router;
 
