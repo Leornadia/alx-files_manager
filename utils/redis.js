384fbf339701d339bb4c1c5497c744fe377589b1
Leornadia <leornadiajose@gmail.com>
2024-12-10 13:40:39 +0200

    redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,34 +1,61 @@
-import redis from 'redis';
-import { promisify } from 'util';
+const redis = require('redis');
 
 class RedisClient {
   constructor() {
+    // Create a Redis client
     this.client = redis.createClient();
-    this.client.on('error', (error) => {
-      console.log(`Redis client not connected to the server: ${error.message}`);
+
+    // Handle Redis client errors
+    this.client.on('error', (err) => {
+      console.error(`Redis error: ${err}`);
     });
   }
 
+  // Check if the Redis client is alive
   isAlive() {
     return this.client.connected;
   }
 
+  // Get a value from Redis by key
   async get(key) {
-    const asyncGet = promisify(this.client.get).bind(this.client);
-    return asyncGet(key);
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (err, value) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(value);
+        }
+      });
+    });
   }
 
+  // Set a value in Redis with an expiration time
   async set(key, value, duration) {
-    const asyncSetex = promisify(this.client.setex).bind(this.client);
-    await asyncSetex(key, duration, value);
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, duration, value, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
   }
 
+  // Delete a value from Redis by key
   async del(key) {
-    const asyncDel = promisify(this.client.del).bind(this.client);
-    await asyncDel(key);
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
   }
 }
 
+// Create and export an instance of RedisClient
 const redisClient = new RedisClient();
-
-export default redisClient;
+module.exports = redisClient;
